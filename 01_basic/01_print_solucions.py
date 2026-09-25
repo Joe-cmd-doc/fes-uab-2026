7faa117596fd7e717de21e42dd664017ee31218d
Marc Esteve Garcia <marc.esteve.garcia@gmail.com>
2026-09-25 08:41:23 +0200

    exercicis

---
+++ 01_basic/01_print_solucions.py
@@ -0,0 +1,15 @@
+###
+# Solucions - print()
+###
+
+# Exercici 1
+# Imprimeix el teu nom i cognom a la consola
+print("Joan Garcia")
+
+# Exercici 2
+# Imprimeix les paraules "Python", "és", "divertit" separades per un guió (-)
+print("Python", "és", "divertit", sep="-")
+
+# Exercici 3
+# Imprimeix el resultat de la suma 15 + 27
+print(15 + 27)
---
+++ 01_basic/01_types_exercicis.py
@@ -0,0 +1,15 @@
+###
+# Exercicis - types()
+# Completa els exercicis següents utilitzant la funció type()
+###
+
+# Exercici 1
+# Imprimeix el tipus del número 25
+
+
+# Exercici 2
+# Imprimeix el tipus del text "Hola món"
+
+
+# Exercici 3
+# Imprimeix el tipus del resultat de la comparació 10 > 5
